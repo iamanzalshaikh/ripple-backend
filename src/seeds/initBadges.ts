7405db944d9abf7c9119c04b49b6a68809093741
SwapnilMk <mswapnil218@gmail.com>
2026-01-04 22:50:50 +0530

    feat: Add badge initialization seed script to populate default badges.

--- src/seeds/initBadges.ts
+++ src/seeds/initBadges.ts
@@ -1,9 +1,9 @@
 // src/seeds/initBadges.ts
-import Badge from '../models/badge.model';
-import { BADGES } from './badges.seed';
+import Badge from "../models/badge.model.js";
+import { BADGES } from "./badges.seed";
 
 export const initBadges = async () => {
-  console.log('🏷️ Initializing badges...');
+  console.log("🏷️ Initializing badges...");
 
   for (const badge of BADGES) {
     const exists = await Badge.findOne({ code: badge.code });
@@ -13,5 +13,5 @@ export const initBadges = async () => {
     }
   }
 
-  console.log('🏁 Badge init done');
+  console.log("🏁 Badge init done");
 };
