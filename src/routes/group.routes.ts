9adb9bc89f8813116dd9964cd9dc2861ed0fd603
Sairaj-Raul <sairaj.raul2001@gmail.com>
2026-01-10 11:24:07 +0530

    Sairaj: Add getGroupChat controller + route

--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -1,6 +1,4 @@
-
-
-import express, { Router } from 'express';
+import express, { Router } from "express";
 import {
   createGroup,
   searchGroups,
@@ -9,21 +7,23 @@ import {
   approveJoinRequest,
   leaveGroup,
   getGroupMembers,
-  deleteGroup
-} from '../controllers/group.controller.js';
-import isAuth from '../middlewares/auth.middleware.js';
+  deleteGroup,
+  getGroupMessages,
+} from "../controllers/group.controller.js";
+import isAuth from "../middlewares/auth.middleware.js";
 
 const router: Router = express.Router();
 
 // router.use(isAuth);
 
-router.post('/', isAuth, createGroup as any);
-router.get('/', isAuth, searchGroups as any);
-router.get('/:id', isAuth, getGroupDetail as any);
-router.post('/:id/join', isAuth, joinGroup as any);
-router.post('/:id/approve/:requestUserId', isAuth, approveJoinRequest as any);
-router.post('/:id/leave', isAuth, leaveGroup as any);
-router.get('/:id/members', isAuth, getGroupMembers as any);
-router.delete('/:id', isAuth, deleteGroup as any);
+router.post("/", isAuth, createGroup as any);
+router.get("/", isAuth, searchGroups as any);
+router.get("/:id", isAuth, getGroupDetail as any);
+router.post("/:id/join", isAuth, joinGroup as any);
+router.post("/:id/approve/:requestUserId", isAuth, approveJoinRequest as any);
+router.post("/:id/leave", isAuth, leaveGroup as any);
+router.get("/:id/members", isAuth, getGroupMembers as any);
+router.get("/:id/messages", isAuth, getGroupMessages as any);
+router.delete("/:id", isAuth, deleteGroup as any);
 
 export default router;
